d2cbe40a1f8172518323e19bcbda7255503e05c7
chucuoi1 <khang140388@yahoo.com>
2022-07-07 01:38:44 +0800

    Update blackhost.sh

--- blackhost.sh
+++ blackhost.sh
@@ -22,40 +22,6 @@ gen64() {
         echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
-
-enable_ipv6() {
-	echo "net.ipv6.conf.default.disable_ipv6=0" >> /etc/sysctl.conf
-	echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
-	# rm -rf /etc/netplan/01-netcfg.yaml
-	echo ADD
-	read ADD
-	echo GW
-	read GW
-	cat > /etc/netplan/01-netcfg.yaml <<EOF
-network:
-    version: 2
-    renderer: networkd
-    ethernets:
-        ens3:
-            dhcp4: yes
-            dhcp6: no
-            addresses:
-                - $ADD::2/64
-            gateway6: $GW::1
-            nameservers:
-                addresses:
-                    - 1.1.1.1
-                    - 8.8.8.8
-                    - 2606:4700:4700::1111
-                    - 2001:4860:4860::8888
-            routes:
-                -   to: $GW::1
-                    scope: link
-EOF
-	sudo netplan apply
-	ping6 -c 10 google.com
-}
-
 install_3proxy() {
     echo "installing 3proxy"
     git clone https://github.com/z3apa3a/3proxy
@@ -122,20 +88,11 @@ EOF
 
 gen_ping () {
 	cat <<EOF
-$(awk -F "/" '{print "" $5 ""}' ${WORKDATA})
+$(awk -F "/" '{print $5}' ${WORKDATA})
 EOF
 }
 
 
-
-# echo IFname
-# ifname=$(read)
-# echo IPV6
-# IPV6=$(read)
-# echo GATEWAY
-# GATEWAY=$(read)
-
-enable_ipv6
 
 echo "installing apps"
 
