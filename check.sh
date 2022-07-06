2152a59b17db6a53f3dffc30131bd923d43418e9
chucuoi1 <khang140388@yahoo.com>
2022-07-06 23:18:09 +0800

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+C=50
+input="ips.txt"
+
+function killCmd() {
+    kill $1
+}
+
+while read ip;
+do
+	ip_log="/tmp/check-${ip}.txt"
+	nohup ping -c $C $ip > $ip_log 2>&1 &
+	serverPID=$!
+
+	echo "Checking IP: $ip "
+
+	count=0
+	status=0
+	until [ $status -gt 0 ]
+	do
+		(( count++ ))
+		printf "%c""."
+
+		if cat $ip_log | grep -q 'ttl='; then
+			status=1
+		fi
+		if tail -n1 $ip_log | grep -q '50 packets'; then
+			status=3
+		fi
+		if [ $count -gt 100 ]; then
+			status=3
+		fi
+		sleep 0.5
+	done
+
+	if [ $status -eq 1 ]; then
+		echo 'OK'
+	else
+		echo 'TIMEOUT!'
+	fi
+
+	kill $serverPID
+	wait $serverPID 2>/dev/null
+
+done < "$input"
